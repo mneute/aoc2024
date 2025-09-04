28cbb1b58c8f8cd13368dd8ffabd1d68d32c7654
mneute <morgan.neute@gmail.com>
2025-09-04 17:50:35 +0200

    Format

--- src/Year2023/Day18/Day18.php
+++ src/Year2023/Day18/Day18.php
@@ -33,28 +33,10 @@ final class Day18 extends AbstractPuzzle
             preg_match(self::REGEX, $line, $matches);
             ['direction' => $direction, 'count' => $count] = $matches;
 
-            $movement = self::DIRECTIONS[$direction];
-
-            for ($i = 1; $i <= $count; $i++) {
-                $newI = $this->currentPosition[0] + $movement[0];
-                $newJ = $this->currentPosition[1] + $movement[1];
-
-                if ($newI > $this->maxI) {
-                    $this->map[$newI] = array_fill(0, $this->maxJ + 1, '.');
-                    $this->maxI = $newI;
-                }
-                if ($newJ > $this->maxJ) {
-                    foreach ($this->map as &$mapLine) $mapLine[$newJ] = '.';
-                    $this->maxJ = $newJ;
-                }
-
-                $this->map[$newI][$newJ] = '#';
-                $this->currentPosition = [$newI, $newJ];
-            }
+            $this->moveCursor($direction, (int) $count);
         }
-        $this->printMap();
 
-        foreach ($this->map as $i => $line) {
+        foreach ($this->map as $line) {
             $str = implode('', $line);
             $first = strpos($str, '#');
             $last = strrpos($str, '#');
@@ -65,14 +47,26 @@ final class Day18 extends AbstractPuzzle
         return new Result($pt1, $pt2);
     }
 
-    private function printMap(): void
+    /**
+     * @param int<1, max> $count
+     */
+    private function moveCursor(string $direction, int $count): void
     {
-        foreach ($this->map as $line) {
-            foreach ($line as $char) {
-                echo $char;
+        for ($i = 1; $i <= $count; $i++) {
+            $newI = $this->currentPosition[0] + self::DIRECTIONS[$direction][0];
+            $newJ = $this->currentPosition[1] + self::DIRECTIONS[$direction][1];
+
+            if ($newI > $this->maxI) {
+                $this->map[$newI] = array_fill(0, $this->maxJ + 1, '.');
+                $this->maxI = $newI;
             }
-            echo PHP_EOL;
+            if ($newJ > $this->maxJ) {
+                foreach ($this->map as &$mapLine) $mapLine[$newJ] = '.';
+                $this->maxJ = $newJ;
+            }
+
+            $this->map[$newI][$newJ] = '#';
+            $this->currentPosition = [$newI, $newJ];
         }
-        echo PHP_EOL;
     }
 }
